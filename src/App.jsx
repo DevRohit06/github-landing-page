4ccf1f505745eb3fbc76ff5c21376ee16d190d4f
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-09-08 22:55:34 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+export default function App(){
+  return (
+    <>
+    <div className="w-full"
+    style={{}}
+    >
+      <Header/>
+    </div>
+    </>
+  )
+}
