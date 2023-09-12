85bc3ea61e96cf7b4b3e1eba461b9e85d552dd12
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-09-12 21:03:45 +0530

    mobile menu ✅

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import Header from "./components/header"
 export default function App(){
   return (
     <>
-    <div className="relative h-screen "
+    <div className="relative h-screen overflow-hidden"
     >
       
       {/* <img className="absolute md:-top-9 sm:-top-24 inset-0 -z-10 w-full h-[2200px] object-cover object md:object-[65%]
       lg:object-[65%] overflow-hidden" src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" style={{}} alt="" /> */}
-      <div className=" max-w-[1280px] overscroll-none mx-auto relative -z-10">
+      <div className=" max-w-[1280px] mx-auto relative -z-10">
         <picture className="">
           <source media="(max-width: 768px)" type="image/webp" srcSet="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg-md.webp 1x, https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp 2x"/>
           <source media="(min-width: 1280px)" type="image/webp" srcSet="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp 1x, https://github.githubassets.com/images/modules/site/home-campaign/hero-bg-2x.webp 2x"/>
 
-          <img alt="" aria-hidden="true" width="4377" className="absolute top-0 pointer-events-none " style={{right: "-1050px", maxWidth: "calc(230vw + 1670px)"}} src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" />
+          <img alt="" aria-hidden="true" width="4377" className="absolute top-0 overflow-hidden pointer-events-none " style={{right: "-1050px", maxWidth: "calc(230vw + 1670px)"}} src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" />
         </picture>
       </div>
       <Header/>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,10 +3,44 @@ import { BsGithub, BsBell } from "react-icons/bs";
 import { LuSearch } from "react-icons/lu";
 import { AiOutlinePlus } from "react-icons/ai";
 import Dropdown from "./dropdown";
-import {HiMenu } from "react-icons/hi"
+import { HiMenu } from "react-icons/hi";
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const MenuList = [
+    {
+      name: "Dashboard",
+      link: "#",
+    },
+    {
+      name: "Pull requests",
+      link: "#",
+    },
+    {
+      name: "Issues",
+      link: "#",
+    },
+    {
+      name: "Codespaces",
+      link: "#",
+    },
+    {
+      name: "Markteplace",
+      link: "#",
+    },
+    {
+      name: "Explore",
+      link: "#",
+    },
+    {
+      name: "Sponsors",
+      link: "#",
+    },
+    {
+      name: "Settings",
+      link: "#",
+    },
+  ];
   const PlusList = [
     {
       name: "Add repository",
@@ -73,17 +107,26 @@ export default function Header() {
   ];
   return (
     <>
-      <header className="px-4 py-4 mx-auto">
-        <div className="">
+      <header className="relative">
+        <div className="px-4 py-4 mx-auto ">
           <div className="flex gap-4 items-center justify-between">
-            <HiMenu className="text-white w-12 h-8 md:hidden block" />
+           <button>
+           <HiMenu
+              onClick={() => {
+                setMobileMenu(!mobileMenu);
+              }}
+              className="text-white w-12 h-8 md:hidden block"
+            />
+           </button>
             <div>
               <BsGithub className="text-white w-8 h-8" />
             </div>
             <button className="hidden rounded-lg md:flex items-center w-96 h-8 bg-gray-900 border-2 border-gray-500">
               <LuSearch className="text-gray-400 ml-2" />
               <div className="flex items-center w-full justify-between px-2">
-                <h1 className="px-2 text-gray-400 text-sm">Search or jump to..</h1>
+                <h1 className="px-2 text-gray-400 text-sm">
+                  Search or jump to..
+                </h1>
                 <div className="py-0.5 px-2 rounded-md text-white border-gray-500 border-2 text-[10px]">
                   /
                 </div>
@@ -91,7 +134,9 @@ export default function Header() {
             </button>
             <div className="md:flex items-center w-full justify-between hidden">
               <div className="flex gap-4 text-white text-sm font-semibold">
-                <a className="flex items-center gap-1" href="">Pull <span className="xl:block hidden"> requests</span></a>
+                <a className="flex items-center gap-1" href="">
+                  Pull <span className="xl:block hidden"> requests</span>
+                </a>
                 <a href="">Issues</a>
                 <a href="">Codespaces</a>
                 <a href="">Marketplace</a>
@@ -115,6 +160,42 @@ export default function Header() {
             <BsBell className="text-white md:hidden block w-12 h-6" />
           </div>
         </div>
+        {
+          mobileMenu ? 
+          <div className="md:hidden block absolute top-0 w-full -z-10 bg-[#24292f] pb-8">
+          <div className="pt-16 ">
+          <button className="mx-auto rounded-lg flex items-center w-5/6 h-8 bg-gray-900 border-2 border-gray-500">
+              <LuSearch className="text-gray-400 ml-2" />
+              <div className="flex items-center w-full justify-between px-2">
+                <h1 className="px-2 text-gray-400 text-sm">
+                  Search or jump to..
+                </h1>
+                <div className="py-0.5 px-2 rounded-md text-white border-gray-500 border-2 text-[10px]">
+                  /
+                </div>
+              </div>
+            </button>
+            <div>
+              <div className="text-white pt-4 font-bold text-sm">
+              {
+                MenuList && MenuList.map((items, index) => {
+                  return (
+                    <>
+                    
+                    <div key={index} className="py-2 px-4 mx-auto flex flex-col justify-center">
+                    <hr className=" border-gray-500 w-full p-1 mx-auto" />
+                      <a className="" href={items.link}>{items.name}</a>
+                    </div>
+                    </>
+                  )
+                })
+              }
+              </div>
+            </div>
+          </div>
+        </div>
+        : null
+        }
       </header>
     </>
   );
